914111c95b09a2c36f1f8a609a1300f425db9728
“Dean” <467271883@qq.com>
2019-07-10 09:01:26 +0800

    format the spec form

--- __tests__/getItemMsg-spec.js
+++ __tests__/getItemMsg-spec.js
@@ -1,18 +1,19 @@
+const getItemMsg = require('../src/getItemMsg')
 
-it('should be true when invoke barcode given barcode = 0001', () => {
+
+
+it('should be msgSpec when invoke getItemMsg given items = items', () => {
     //given
-    const barcode = "0001";
+    const items = [{"id": "0001", "name" : "Coca Cola", "price": 3, "count" : 1},
+        {"id": "0003", "name" : "Pepsi-Cola", "price": 5,"count": 2},
+        {"id": "0005", "name" : "Dr Pepper", "price": 7, "count": 1}]
     //when
-    const result = isBarcodeValid(barcode)
+    const result = getItemMsg(items)
     //then
-    expect(result).toBe(true);
+    const msgSpec =
+        "Coca Cola                       3          1\n" +
+        "Pepsi-Cola                       5          2\n" +
+        "Dr Pepper                       7          1";
+    expect(result).toBe(msgSpec)
 });
 
-it('should be ErrorMsg when invoke barcode given barcode = 0001a', () => {
-    //given
-    const barcode = "0001a";
-    //when
-    const result = isBarcodeValid(barcode)
-    //then
-    expect(result).toBe("[Error]: barcode" + barcode + "isInvalid");
-});
--- src/getItemMsg.js
+++ src/getItemMsg.js
@@ -1,7 +1,7 @@
 function getItemMsg(items) {
     let result = "";
     for(let i = 0;i < items.length;i++){
-        result += items[i].name + "                       " + items[i].price + "          " + items.count;
+        result += items[i].name + "                       " + items[i].price + "          " + items[i].count;
         if(i !== items.length - 1){
             result += "\n";
         }
